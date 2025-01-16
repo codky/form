19be096b391234654583baf5429ba093516016bb
minseok kyeon <msky0912@gmail.com>
2025-01-16 10:36:55 +0900

    체크 박스 - 멀티
     - th:for="${#ids.prev('regions')}"
       -> 멀티 체크박스는 같은 이름의 여러 체크박스를 만들 수 있다. 반복해서 HTML 태그를 생성할 때, 생성된 HTML 태그 속성에서 name은 같아도 되지만, id는 모두 달라야 한다. 타임리프는 체크박스를 each 루프안에서 반복해서 만들 때 임의로 1,2,3 숫자를 뒤에 붙여준다.
      - regions_가 체크박스 숫자만큼 생성되는 것은 타임리프가 옵션 수 만큼 생성하는 것이니 무시하면 된다.
      - 멀티 체크 박스에서 regions를 선택해서 저장하면, 조회시에 checked 속성이 추가된다. 타임리프는 th:field에 지정한 값과 th:value의 값을 비교해서 체크를 자동으로 처리해준다.

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,20 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /*
+    등록 폼, 상세화면, 수정 폼에서 모두 서울,부산,제주 라는 체크박스를 반복해서 보여줘야해서 각각 컨트롤러에서 반복해서 넣어줘야한다.
+    @ModelAttribute는 컨트롤러에 있는 별도의 메서드에 적용할 수 있다.
+    해당 컨트롤러를 요청할 때 regions에서 반환한 값이 자동으로 모델(model)에 담기게 된다.
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -42,6 +58,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
